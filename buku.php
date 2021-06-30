fe9ae8825a0e91570af74642e501f6869a3c34f8
MeNavel <information.navel@gmail.com>
2021-06-30 13:45:44 +0700

    update buku

--- buku.php
+++ buku.php
@@ -1,15 +1,9 @@
 <?php
-    public function update(ProfileRequest $request)
-    {
-        auth()->user()->update($request->all());
-
-        return back()->withStatus(__('Profile successfully updated.'));
-    }
-
-    public function password(PasswordRequest $request)
-    {
-        auth()->user()->update(['password' => Hash::make($request->get('password'))]);
-
-        return back()->withPasswordStatus(__('Password successfully updated.'));
-    }
+    $find = $result->created_at;
+    $output = DB::table('results')->where('created_at', $find)->first();
+    $tgl = $output->created_at;
+    $foto = $output->image;
+    $hasil = $output->status;
+    $indentity = DB::table('datasets')->where('kode', $result->kode)->first();
+    return view('pages.result_negatif', ['id' => $indentity ,'tgl' => $tgl ,'foto' => $foto ,'result' => $hasil]);
 ?>
