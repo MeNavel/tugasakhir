b9941693fb20bea1fcbee7f2694fba7dee6148ee
MeNavel <information.navel@gmail.com>
2021-06-30 13:11:04 +0700

    update buku

--- buku.php
+++ buku.php
@@ -1,15 +1,15 @@
 <?php
     $check_face = file_get_contents("http://127.0.0.1:5000/predict_face?file=".$file_predict."");
-    if($check_face == "Tidak Terdeteksi"){
-        $nama = "Orang";
-        $info = "Wajah tidak tersedia pada dataset";
+    if($check_face != "Tidak Terdeteksi"){
+        $$indentity = DB::table('datasets')->where('kode', 'like', "%".$check_face."%")->first();
         DB::table('results')->insert([
-            'nama' => $nama,
+            'nama' => $indentity->nama,
             'status' => $result,
+            'kode' => $check_face,
             'image' => $fileName,
             'created_at' => $tgl,
             'id_akun' => $id
         ]);
-        return view('pages.result_positive', ['foto' => $fileName, 'result' => $result, 'tgl' => $tgl, 'info' => $info]);
+        return view('pages.result_negatif', ['id' => $indentity ,'tgl' => $tgl ,'foto' => $fileName ,'result' => $result]);
     }
 ?>
