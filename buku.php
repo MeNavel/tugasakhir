2309d0a98cf81292afa318869d94f7dd9759d581
MeNavel <information.navel@gmail.com>
2021-06-30 12:56:57 +0700

    update buku melihat hasil deteksi

--- buku.php
+++ buku.php
@@ -1,18 +1,5 @@
 <?php
-    $request->file->storeAs('public',$fileName);    
-    $file_predict = "/Applications/XAMPP/xamppfiles/htdocs/tugasakhir/storage/app/public/".$fileName;
-    $check_mask = file_get_contents("http://127.0.0.1:5000/predict_mask?file=".$file_predict."");
-    if($check_mask == "mask"){
-        $result = "Menggunakan Masker";
-        $nama = "Orang";
-        $info = "Orang Tersebut Telah Menaati Protokol Kesehatan";
-        DB::table('results')->insert([
-            'nama' => $nama,
-            'status' => $result,
-            'image' => $fileName,
-            'created_at' => $tgl,
-            'id_akun' => $id
-        ]);
-        return view('pages.result_positive', ['foto' => $fileName, 'result' => $result, 'tgl' => $tgl, 'info'=>$info]);
-    }
+    $id = auth()->user()->id;
+    $results = Result::where('id_akun', $id)->latest()->SimplePaginate(6);
+    return view('pages.result',compact('results'))->with('i', (request()->input('page', 1) - 1) * 5);
 ?>
